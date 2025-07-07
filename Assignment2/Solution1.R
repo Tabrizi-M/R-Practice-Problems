26a288cd8edf3ed2234e3ba0c114a9e06b337c45
Hi there 👋 I'm Mandana <mandana1234@gmail.com>
2025-07-07 15:17:00 -0400

    Update Solution1.R

--- Assignment2/Solution1.R
+++ Assignment2/Solution1.R
@@ -1,37 +1,34 @@
+### Solution to exercise 4.2 of the textbook
 
 require(dobson)
 require(ggplot2)
 
-#preparation of data
+### preparation of data
 leukemia <- dobson::leukemia
 log_wbc = leukemia$wbc
 time = leukemia$time
 log_time = log(leukemia$time)
 
-#plot
+### plot of data
 plot(y=leukemia$time, x=leukemia$wbc, pch=19, col= "black",
      main ="Survival time for leukemia", ylab ="time to death (weeks)", xlab = "log(white blood cell count)")
 
-#models
+### models
 lm1 <- lm(log_time ~ log_wbc)
-summary(lm1)
-
 glm1 <- glm(time ~ log_wbc, family="poisson")
-summary(glm1)
 
-#table
+### table of results
 table1 <- rbind( x=log_wbc, y= glm1$y, fitted_values= glm1$fitted.values, 
                  residuals = resid(glm1), standard_residuals = glm1$residuals)
 write.csv(x= table1, file = "./York Statistics/Math 6622 - GLM/A2/table.csv" )
 
-#plots
+### plots for results
 plot(y=leukemia$time, x=leukemia$wbc, pch=19, col= "black",
      main ="Survival time for leukemia (red=lm, blue=glm)", xlab ="time to death (weeks)", ylab = "log(white blood cell count)")
 lines(leukemia$wbc, glm1$fitted, col="blue",lwd=3)
 lines(leukemia$wbc, exp(lm1$fitted.values), col="red",lwd=3)
 
-#residual plot
+### residual plot
 plot(glm1$residuals , pch=19, ylab = "residuals", main = "residual plot")
 abline(h = 0)
-
 plot( glm1$fitted , resid(glm1), pch=19)
